87d0fceb394667033ef20e51bd469aaaf16f4c4c
OYanEnrique <enrique4work@gmail.com>
2025-09-13 00:05:26 -0300

    feat: Add 50 new unique candy-themed enemies, bosses, and adjectives for regular and boss creatures

--- characters.py
+++ characters.py
@@ -8,7 +8,7 @@ def create_enemy(level):
 	
 	enemy_hp = 15 + level
 	
-	names = [
+	names = list({
 		"Gelatimon", "Chocobite", "Marshmunch", "Gummyfang", "Caramellow", "Fudgester", "Sprinkletooth", "Nougator", "Puffmallow", "Licoricepike",
 		"Bonbonic", "Truffluff", "Sugaraptor", "Candicorn", "Mochibite", "Taffyjaw", "Jellifin", "Frostmallow", "Cinnasnarl", "Mallowtail",
 		"Sourpuff", "Twizzlurk", "Crumblepaw", "Snickerdash", "Cocoaclaw", "Meringoon", "Gumdropper", "S'moregon", "Frostfang", "Pecanine",
@@ -18,9 +18,14 @@ def create_enemy(level):
 		"Sourling", "Twizzlizard", "Crumblebee", "Snickerbat", "Cocoamole", "Meringull", "Gumdrake", "S'morebat", "Frostowl", "Pecatoad",
 		"Maplemouse", "Crispbat", "Canelizard", "Brittlebee", "Fudgebat", "Mochifin", "Pufferbat", "Cinnacorn", "Syrupbat", "Glazebee",
 		"Mallowbat", "Tartbat", "Crumblefin", "Jellyfin", "Nougatcat", "Sprinklecat", "Sugarcat", "Marshbat", "Frostcat", "Cocoacat",
-		"Gumcat", "Trufflecat", "Bonboncat", "Candycat", "Mochicat", "Taffycat", "Jellicat", "Frostcat", "Cinnacat", "Mallowcat"
-	]
-	adjectives = [
+		"Gumcat", "Trufflecat", "Bonboncat", "Candycat", "Mochicat", "Taffycat", "Jellicat", "Frostcat", "Cinnacat", "Mallowcat",
+		"Marshmouse", "Jellyhopper", "Nougathog", "Fudgefin", "Taffybug", "Chocoroo", "Sprinklebat", "Bonbonbunny", "Truffletail", "Frostfox",
+		"Cinnacub", "Mallowmole", "Syrupbee", "Caramelpup", "Crunchycat", "Glazebird", "Jellysnail", "Mochifin", "Taffycrab", "Fudgelamb",
+		"Licoriceowl", "Sugarfrog", "Gummysheep", "Bonbonrat", "Truffledeer", "Frostferret", "Cinnadillo", "Mallowlizard", "Syrupduck", "Carameltoad",
+		"Crunchycoon", "Glazebee", "Jellyhog", "Mochifox", "Taffyfish", "Fudgefrog", "Licoricecat", "Sugarbug", "Gummygoat", "Bonbonmouse",
+		"Trufflehare", "Frostmouse", "Cinnadog", "Mallowbee", "Syrupgoose", "Caramelmoth", "Crunchymouse", "Glazebat", "Jellycrab", "Gumdropper"
+	})
+	adjectives = list({
 		"of Sugar", "Frosted", "Caramelized", "Filled", "from the Candyshop", "Glazed", "Crunchy", "Swirled", "Sticky", "Whipped",
 		"Candied", "Toasted", "Iced", "Molten", "Syrupy", "Spiced", "Crisped", "Dusted", "Layered", "Fluffy",
 		"Gooey", "Crumbly", "Nutty", "Zesty", "Tangy", "Buttery", "Jammed", "Honeyed", "Roasted", "Sprinkled",
@@ -29,12 +34,29 @@ def create_enemy(level):
 		"Sparkling", "Glossy", "Shiny", "Dazzling", "Twisted", "Swirled", "Layered", "Frosty", "Creamy", "Dreamy",
 		"Magic", "Mystic", "Enchanted", "Royal", "Epic", "Legendary", "Ancient", "Modern", "Classic", "Wild",
 		"Mini", "Mega", "Ultra", "Hyper", "Super", "Max", "Prime", "Alpha", "Omega", "Beta",
-		"Sweetest", "Sour", "Salty", "Spicy", "Mild", "Bold", "Gentle", "Fierce", "Brave", "Clever"
-	]
-	boss_names = [
-		"Lord Licorice", "Queen Caramel", "Duke Doughnut", "Princess Praline", "Baroness Bonbon", "Count Candy", "Emperor Eclair", "King Kandy", "Sir Sugar", "Madame Meringue"
-	]
-	boss_adjectives = [ "The Sour", "The Sweet", "The Spicy", "The Creamy", "The Crunchy", "The Chewy", "The Fluffy", "The Gooey", "The Frosted", "The Glazed"]
+		"Sweetest", "Sour", "Salty", "Spicy", "Mild", "Bold", "Gentle", "Fierce", "Brave", "Clever",
+		"Sugary", "Frosty", "Caramel-Coated", "Glazed", "Crunch-Coated", "Swirled", "Sticky", "Whipped", "Candied", "Toasted",
+		"Iced", "Molten", "Syrupy", "Spiced", "Crispy", "Dusted", "Layered", "Fluffy", "Gooey", "Crumbly",
+		"Nutty", "Zesty", "Tangy", "Buttery", "Jammed", "Honeyed", "Roasted", "Sprinkled", "Melted", "Puffed",
+		"Brittle", "Chewy", "Chilled", "Spun", "Velvety", "Rich", "Light", "Rainbow", "Golden", "Silvered",
+		"Bronzed", "Crystalized", "Powdered", "Glittering", "Sparkling", "Glossy", "Shiny", "Dazzling", "Twisted", "Creamy"
+	})
+	boss_names = list({
+		"Lord Licorice", "Queen Caramel", "Duke Doughnut", "Princess Praline", "Baroness Bonbon", "Count Candy", "Emperor Eclair", "King Kandy", "Sir Sugar", "Madame Meringue",
+		"King Jawbreaker", "Baroness Bonbon", "Duke Nougat", "Emperor Eclair", "Count Candyfloss", "General Gumdrop", "Captain Caramel", "Sir Sugarcane", "Lady Lollipop", "Master Mochi",
+		"Archduke Almond", "Supreme S'more", "Overlord Oreo", "Grandmaster Gummy", "Highlord Honeycomb", "Commander Cocoa", "Warlord Wafer", "Pharaoh Fudge", "Sultan Snickers", "Oracle Orangepeel",
+		"Titan Toffee", "Vizier Velvet", "Magister Maple", "Chancellor Cherry", "Regent Raspberry", "Viceroy Vanilla", "Shogun Shortbread", "Paladin Peanut", "Sorcerer Sherbet", "Guardian Gum",
+		"Sentinel Sprinkles", "Enchanter Espresso", "Prophet Pistachio", "Mystic Meringue", "Sage Strawberry", "Seer Sours", "Judge Jellybean", "Marshal Mint", "Admiral Apple", "Bishop Brownie",
+		"Warden Walnut", "Keeper Kiwi", "Watcher Whitechoc", "Herald Hazelnut", "Champion Churro", "Defender Dulce", "Conqueror Crunch", "Queen Marshmallow", "Princess Praline", "Count Candy"
+	})
+	boss_adjectives = list({
+		"The Sour", "The Sweet", "The Spicy", "The Creamy", "The Crunchy", "The Chewy", "The Fluffy", "The Gooey", "The Frosted", "The Glazed",
+		"The Supreme Sweet", "The Infinite Sugar", "The Eternal Frost", "The Unyielding Caramel", "The Vicious Nougat", "The Colossal Marshmallow", "The Unbreakable Toffee", "The Legendary Licorice", "The Fearless Fudge", "The Unstoppable Gummy",
+		"The Mighty Jawbreaker", "The Dreaded Bonbon", "The Glorious Praline", "The Ruthless Choco", "The Invincible S'more", "The Cursed Candyfloss", "The Blessed Maple", "The Ancient Almond", "The Timeless Honeycomb", "The Radiant Raspberry",
+		"The Shadowed Sherbet", "The Blazing Brownie", "The Frozen Meringue", "The Electric Sherbet", "The Toxic Pistachio", "The Savage Shortbread", "The Noble Peanut", "The Fierce Sprinkles", "The Prime Espresso", "The Alpha Apple",
+		"The Omega Oreo", "The Beta Berry", "The Sweetest Churro", "The Sour Sours", "The Salty Walnut", "The Spicy Hazelnut", "The Mild Kiwi", "The Bold Dulce", "The Gentle Whitechoc", "The Brave Crunch",
+		"The Clever Jellybean", "The Wise Mint", "The Merciless Strawberry", "The Just Vanilla", "The Loyal Gum", "The Proud Wafer", "The Silent Judge", "The Loud Champion", "The Swift Keeper", "The Patient Watcher"
+	})
 
 	if level % 10 == 0:
 		print(f'🎉🎉🎉 Candy Boss Level [bold magenta]{level}[/bold magenta] Reached! 🎉🎉🎉')
--- game_logic.py
+++ game_logic.py
@@ -2,6 +2,7 @@ from random import choice
 from game_utils import show_status
 from time import sleep
 from rich import print
+from random import randint
 
 def battle(p, e):
 	sleep(2)
@@ -14,15 +15,21 @@ def battle(p, e):
 		choice_input = input(f'Choose your attack (1 or 2)\n').strip()
 
 		if choice_input == '1':
-			damage_dealt = p['attacks'][0]['damage']
-			e['current_hp'] -= damage_dealt
-			print(f'[bold blue]You used [/bold blue][bold red]{p['attacks'][0]['attack_name']}[/bold red] [bold blue]and dealt[/bold blue] [bold red]{damage_dealt}[/bold red] [bold blue]damage[/bold blue]')
-			sleep(1)
+			if randint(1, 100) <= 15:
+				print(f'[bold red]Your Attack missed![/bold red]')
+			else:
+				damage_dealt = p['attacks'][0]['damage']
+				e['current_hp'] -= damage_dealt
+				print(f'[bold blue]You used [/bold blue][bold red]{p['attacks'][0]['attack_name']}[/bold red] [bold blue]and dealt[/bold blue] [bold red]{damage_dealt}[/bold red] [bold blue]damage[/bold blue]')
+				sleep(1)
 		elif choice_input == '2':
-			damage_dealt = p['attacks'][1]['damage']
-			e['current_hp'] -= damage_dealt
-			print(f'[bold blue]You used[/bold blue] [bold red]{p['attacks'][1]['attack_name']}[/bold red] [bold blue]and dealt[/bold blue] [bold red]{damage_dealt}[/bold red] [bold blue]damage[/bold blue]')
-			sleep(1)
+			if randint(1, 100) <= 15:
+				print(f'[bold red]Your Attack missed![/bold red]')
+			else:
+				damage_dealt = p['attacks'][1]['damage']
+				e['current_hp'] -= damage_dealt
+				print(f'[bold blue]You used[/bold blue] [bold red]{p['attacks'][1]['attack_name']}[/bold red] [bold blue]and dealt[/bold blue] [bold red]{damage_dealt}[/bold red] [bold blue]damage[/bold blue]')
+				sleep(1)
 		else:
 			print(f'[bold red]Invalid option[/bold red]')
 			continue
@@ -34,12 +41,15 @@ def battle(p, e):
 			return True
 		else:
 			if choice_input in ['1', '2']:
-				print(f"[bold yellow]------ {e['name']} TURN ------[/bold yellow]")
-				enemy_attack = choice(e['attacks'])
-				enemy_damage = enemy_attack['damage']
-				p['current_hp'] -= enemy_damage
-				print(f"{e['name']} [bold blue]used[/bold blue] [bold red]{enemy_attack['attack_name']}[/bold red] [bold blue]and dealt[/bold blue] [bold red]{enemy_damage}[/bold red] [bold blue]damage[/bold blue]")
-				sleep(1)
+				if randint(1, 100) <= 15:
+					print(f'{e['name']} [bold red]Attack missed![/bold red]')
+				else:
+					print(f"[bold yellow]------ {e['name']} TURN ------[/bold yellow]")
+					enemy_attack = choice(e['attacks'])
+					enemy_damage = enemy_attack['damage']
+					p['current_hp'] -= enemy_damage
+					print(f"{e['name']} [bold blue]used[/bold blue] [bold red]{enemy_attack['attack_name']}[/bold red] [bold blue]and dealt[/bold blue] [bold red]{enemy_damage}[/bold red] [bold blue]damage[/bold blue]")
+					sleep(1)
 				if p['current_hp'] <= 0:
 					p['current_hp'] = 0
 					return False
